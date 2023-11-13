7580109079a6d7756d2953ecbd29cdeffae22b38
Laurensia Audrey <18221114@std.stei.itb.ac.id>
2023-11-14 06:37:37 +0700

    fix auth

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+# Use the official Python image from the Docker Hub
+FROM python:3
+
+# Set the working directory inside the container
+ADD main.py .
+
+# Copy the current directory contents into the container at /app
+COPY . /godeliv_TST
+WORKDIR /godeliv_TST
+COPY ./requirements.txt /godeliv_TST/requirements.txt
+# Install any necessary dependencies
+RUN pip install --no-cache-dir --upgrade -r /godeliv_TST/requirements.txt  
+
+# Command to run the FastAPI server when the container starts
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80"]
---
+++ main.py
@@ -0,0 +1,201 @@
+from fastapi import FastAPI, HTTPException, Depends, status, APIRouter, Query
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+import json
+import jwt
+import uvicorn
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from passlib.hash import bcrypt
+
+with open('data.json', 'r') as file:
+    data = json.load(file)
+    
+auth = APIRouter(tags=["auth"],)
+menu = APIRouter(tags=["menu"],)
+user = APIRouter(tags=["user"],)
+
+recommendation_router=APIRouter(tags=["recommendation"],)
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    return "Welcome to Your Personalized Menu Recommendation!"
+
+class User(BaseModel):
+    id_user: int
+    nama_user: str
+    umur_user: int
+    target: str
+
+class Recommendation(BaseModel):
+    id_user: int
+    id_menu: int
+    date: str
+
+class Menu(BaseModel):
+    id_menu: int
+    nama_menu: str
+    kalori: int
+    target: str
+
+class signin_user:
+    def __init__(self, id, username, pass_hash):
+        self.id = id
+        self.username = username
+        self.pass_hash = pass_hash
+
+    def verify_password(self, password):
+        return bcrypt.verify(password, self.pass_hash)
+
+def write_data(data):
+    with open("data.json", "w") as write_file:
+        json.dump(data, write_file, indent=4)
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+recommendation = FastAPI()
+JWT_SECRET = 'myjwtsecret'
+ALGORITHM = 'HS256'
+
+def get_user_by_username(username):
+    for desain_user in data['signin_user']:
+        if desain_user['username'] == username:
+            return desain_user
+    return None
+
+def authenticate_user(username: str, password: str):
+    user_data = get_user_by_username(username)
+    if not user_data:
+        return None
+
+    user = signin_user(id=user_data['id'], username=user_data['username'], pass_hash=user_data['pass_hash'])
+
+    if not user.verify_password(password):
+        return None
+
+    return user
+
+
+@auth.post('/token')
+async def generate_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(form_data.username, form_data.password)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, 
+            detail='Invalid username or password'
+        )
+
+    token = jwt.encode({'id': user.id, 'username': user.username}, JWT_SECRET, algorithm=ALGORITHM)
+
+    return {'access_token': token, 'token_type': 'bearer'}
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
+        user = get_user_by_username(payload.get('username'))
+        return signin_user(id=user['id'], username=user['username'], pass_hash=user['pass_hash'])
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, 
+            detail='Invalid username or password'
+        )
+
+@auth.post('/signin_user')
+async def create_user(username: str, password: str):
+    last_user_id = data['signin_user'][-1]['id'] if data['signin_user'] else 0
+    user_id = last_user_id + 1
+    user = jsonable_encoder(signin_user(id=user_id, username=username, pass_hash=bcrypt.hash(password)))
+    data['signin_user'].append(user)
+    write_data(data)
+    return {'message': 'User created successfully'}
+
+@auth.get('/signin_user/me')
+async def get_user(user: signin_user = Depends(get_current_user)):
+    return {'id': user.id, 'username': user.username}
+
+@menu.get("/get_menu")
+def get_menu(user: signin_user = Depends(get_current_user)):
+    return data["menu"]
+
+
+@user.get("/get_user")
+def get_user(user: signin_user = Depends(get_current_user)):
+    return data["user"]
+
+
+@menu.put("/update_menu")
+async def update_menu(id_menu: int, nama_menu: str, kalori: int, target: str, user: signin_user = Depends(get_current_user)):
+    for menu in data["menu"]:
+        if menu["id_menu"] == id_menu:
+            menu["nama_menu"] = nama_menu
+            menu["kalori"] = kalori
+            menu["target"] = target
+            with open('data.json', 'w') as outfile:
+                json.dump(data, outfile, indent=4)
+            return {"message": "Menu updated successfully"}
+    return {"error": "Menu not found"}
+
+
+@menu.delete("/delete_menu")
+async def delete_menu(id_menu: int, user: signin_user = Depends(get_current_user)):
+    for menu in data["menu"]:
+        if menu["id_menu"] == id_menu:
+            data["menu"].remove(menu)
+            with open('data.json', 'w') as outfile:
+                json.dump(data, outfile, indent=4)
+            return {"message": "Menu deleted successfully"}
+    return {"error": "Menu not found"}
+
+
+@menu.post("/add_menu")
+async def add_menu(id_menu: int, nama_menu: str, kalori: int, user: signin_user = Depends(get_current_user)):
+    menu_id = {menu["id_menu"] for menu in data["menu"]}
+    if id_menu in menu_id:
+        raise HTTPException(status_code=400, detail="ID already exists")
+    menu_baru = {"id_menu": id_menu, "nama_menu": nama_menu, "kalori": kalori}
+    data["menu"].append(menu_baru)
+    with open('data.json', 'w') as outfile:
+        json.dump(data, outfile, indent=4)
+    return {"message": "Menu berhasil ditambahkan"}
+
+
+@user.post("/add_user")
+async def add_user(id_user: int, nama_user: str, jenis_kelamin: str, umur_user: int, user: signin_user = Depends(get_current_user)):
+    user_id = {user["id_user"] for user in data["user"]}
+    if id_user in user_id:
+        raise HTTPException(status_code=400, detail="ID already exists")
+    new_user = {"id_user": id_user, "nama_user": nama_user, "jenis_kelamin": jenis_kelamin, "umur_user": umur_user}
+    data["user"].append(new_user)
+    with open('data.json', 'w') as outfile:
+        json.dump(data, outfile, indent=4)
+    return {"message": "User berhasil ditambahkan"}
+
+@recommendation_router.get("/get_recommendation")
+async def get_recommendation(user_id: int, current_user: signin_user = Depends(get_current_user)):
+    # Find user details
+    user = next((u for u in data['user'] if u['id_user'] == user_id), None)
+
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    # Basic recommendation logic based on gender and age
+    gender = user['jenis_kelamin'].lower()
+    age = user['umur_user']
+
+    # Example recommendation logic: Recommend a meal with kalori <= 300 for females under 35, and <= 500 for males under 35
+    if gender == 'perempuan' and age < 35:
+        recommended_meals = [meal for meal in menu if meal['kalori'] <= 300]
+    elif gender == 'laki-laki' and age < 35:
+        recommended_meals = [meal for meal in menu if meal['kalori'] <= 500]
+    else:
+        # Default recommendation for other cases
+        recommended_meals = menu
+
+    return {"user": user, "recommended_meals": recommended_meals}
+
+app.include_router(auth)
+app.include_router(menu)
+app.include_router(user)
+app.include_router(recommendation_router)
+
+# if __name__	=="__main__":	
+#     uvicorn.run("main:app",	host="localhost",port=8006, reload=True)
